bcb7242853da9f5871d9990ca00ef21b7507162b
devKarNick <karabanov.nikolai@bk.ru>
2023-06-03 14:03:33 +0300

    tabs

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,35 @@ if (videoBlock) {
     playBtn.classList.remove('video-block__play--played')
   };
 }
+
+/* Tabs */
+
+
+const toBookTabsNav = document.querySelector('.to-book-tabs-nav');
+const toBookTabsBtns = document.querySelectorAll('.to-book-tabs-nav__btn');
+const toBookTabsItems = document.querySelectorAll('.to-book-tabs__item');
+const toBookTabsItemsVisible = document.querySelectorAll('.to-book-tabs__item--visible');
+
+
+
+if (toBookTabsNav) {
+
+  toBookTabsNav.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.classList.contains('to-book-tabs-nav__btn')) {
+      const path = target.dataset.path;
+
+      toBookTabsBtns.forEach(el => { el.classList.remove('to-book-tabs-nav__btn--active') });
+      target.classList.add('to-book-tabs-nav__btn--active');
+
+      toBookTabsItems.forEach(el => {
+        el.classList.remove('to-book-tabs__item--visible');
+      });
+
+      document.querySelectorAll(`[data-target="${path}"]`).forEach(el => {
+        el.closest('.to-book-tabs__item').classList.add('to-book-tabs__item--visible');
+      });
+
+    }
+  });
+}
