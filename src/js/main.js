10a5e2e2c8b3dfa4b5519259a8f2a32247875ced
Maksim Popov <makso16108@gmail.com>
2023-06-04 22:58:00 +0300

    Подправил код и написал комментарии.

--- src/js/main.js
+++ src/js/main.js
@@ -6,55 +6,65 @@ import './_components';
 
 
 /* Video block */
+// лучше дождаться, когда страница загрузится и только тогда выполнять скрипты и вешать обработчики
+window.onload = () => {
+  const videoBlock = document.querySelector('.video-block');
 
-const videoBlock = document.querySelector('.video-block');
+  if (videoBlock) {
+    const video = videoBlock.querySelector('video');
+    const playBtn = videoBlock.querySelector('.video-block__play');
 
-if (videoBlock) {
-  const video = videoBlock.querySelector('video');
-  const playBtn = videoBlock.querySelector('.video-block__play');
+    playBtn.addEventListener('click', () => {
+      videoBlock.classList.add('video-block--played');
+      video.play();
+      video.controls = true;
+      playBtn.classList.add('video-block__play--played');
+    });
 
-  playBtn.addEventListener('click', () => {
-    videoBlock.classList.add('video-block--played');
-    video.play();
-    video.controls = true;
-    playBtn.classList.add('video-block__play--played');
-  });
+    video.onpause = function () {
+      videoBlock.classList.remove('video-block--played');
+      video.contains = false;
+      playBtn.classList.remove('video-block__play--played')
+    };
+  }
 
-  video.onpause = function () {
-    videoBlock.classList.remove('video-block--played');
-    video.contains = false;
-    playBtn.classList.remove('video-block__play--played')
-  };
-}
-
-/* Tabs */
-
-
-const toBookTabsNav = document.querySelector('.to-book-tabs-nav');
-const toBookTabsBtns = document.querySelectorAll('.to-book-tabs-nav__btn');
-const toBookTabsItems = document.querySelectorAll('.to-book-tabs__item');
-const toBookTabsItemsVisible = document.querySelectorAll('.to-book-tabs__item--visible');
+  /* Tabs */
 
 
+  const toBookTabsNav = document.querySelector('.to-book-tabs-nav');
+  const toBookTabsBtns = document.querySelectorAll('.to-book-tabs-nav__btn');
+  const toBookTabsItems = document.querySelectorAll('.to-book-tabs__item');
+  const toBookTabsItemsVisible = document.querySelectorAll('.to-book-tabs__item--visible');
 
-if (toBookTabsNav) {
 
-  toBookTabsNav.addEventListener('click', (e) => {
-    const target = e.target;
-    if (target.classList.contains('to-book-tabs-nav__btn')) {
-      const path = target.dataset.path;
 
-      toBookTabsBtns.forEach(el => { el.classList.remove('to-book-tabs-nav__btn--active') });
-      target.classList.add('to-book-tabs-nav__btn--active');
+  if (toBookTabsNav) {
+    // нужно вешать обработчик событий на сами кнопки, а не на их контейнер
+    // без использования JQuery это нужно делать в цикле
+    for (const btn of toBookTabsBtns) {
+      btn.addEventListener('click', (e) => {
+        // отменяем стандартное поведение кнопки при клике
+        e.preventDefault();
 
-      toBookTabsItems.forEach(el => {
-        el.classList.remove('to-book-tabs__item--visible');
+        // Проверяем, если кликнули на кнопку, то берем target, если кликнули на дочерний элемент кнопки,
+        // то ищем родителя с классом to-book-tabs-nav__btn, то есть кнопку и возвращаем ее. Так у нас всегда по клику будет получена кнопка
+        const target = e.target.classList.contains('to-book-tabs-nav__btn') ? e.target : e.target.closest('.to-book-tabs-nav__btn');
+
+        const path = target.dataset.path;
+
+        toBookTabsBtns.forEach(el => { el.classList.remove('to-book-tabs-nav__btn--active') });
+        target.classList.add('to-book-tabs-nav__btn--active');
+
+        toBookTabsItems.forEach(el => {
+          el.classList.remove('to-book-tabs__item--visible');
+        });
+
+        document.querySelectorAll(`[data-target="${path}"]`).forEach(el => {
+          el.closest('.to-book-tabs__item').classList.add('to-book-tabs__item--visible');
+        });
       });
-
-      document.querySelectorAll(`[data-target="${path}"]`).forEach(el => {
-        el.closest('.to-book-tabs__item').classList.add('to-book-tabs__item--visible');
-      });
-
     }
-  });
+  }
+
 }
+
